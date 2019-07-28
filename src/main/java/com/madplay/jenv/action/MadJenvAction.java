5e0d35fbecf3a392a519ac2b75d0095f0e8509d7
madplay <itsmetaeng@gmail.com>
2019-07-28 14:29:20 +0900

    [#6] bug: if the jEnv is not installed, it does not work

--- src/main/java/com/madplay/jenv/action/MadJenvAction.java
+++ src/main/java/com/madplay/jenv/action/MadJenvAction.java
@@ -7,12 +7,15 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.ProjectJdkTable;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.projectRoots.impl.SdkConfigurationUtil;
-import com.madplay.jenv.MadDialogWrapper;
+import com.madplay.jenv.DefaultDialog;
+import com.madplay.jenv.MadJenvDialog;
 import com.madplay.jenv.MadJenvHelper;
 
+import java.awt.Desktop;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URI;
 
 /**
  * @author kimtaeng
@@ -22,16 +25,29 @@ public class MadJenvAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        MadDialogWrapper dialogWrapper = new MadDialogWrapper();
-        dialogWrapper.show();
+        if (MadJenvHelper.isJenvInstalled()) {
+            MadJenvDialog dialogWrapper = new MadJenvDialog();
+            dialogWrapper.show();
 
-        if (dialogWrapper.isOK()) {
-            String selectedVersion = MadJenvHelper.getSelectedJavaVersion();
-            Sdk jdk = ProjectJdkTable.getInstance().findJdk(selectedVersion);
-            Project project = e.getData(DataKeys.PROJECT);
-            changeJenvVersion();
-            SdkConfigurationUtil.setDirectoryProjectSdk(project, jdk);
-            MadJenvHelper.setCurrentJavaVersion(selectedVersion);
+            if (dialogWrapper.isOK()) {
+                String selectedVersion = MadJenvHelper.getSelectedJavaVersion();
+                Sdk jdk = ProjectJdkTable.getInstance().findJdk(selectedVersion);
+                Project project = e.getData(DataKeys.PROJECT);
+                changeJenvVersion();
+                SdkConfigurationUtil.setDirectoryProjectSdk(project, jdk);
+                MadJenvHelper.setCurrentJavaVersion(selectedVersion);
+            }
+        } else {
+            DefaultDialog defaultDialog = new DefaultDialog();
+            defaultDialog.show();
+
+            if(defaultDialog.isOK()) {
+                try {
+                    Desktop.getDesktop().browse(new URI("https://www.jenv.be/"));
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
         }
     }
 
